4be857d31e0cd89d844b4c6282dc6fbee74e2309
Nihal Konda <nihal.konda@sjsu.edu>
2020-03-20 04:01:34 -0700

    Initial Commit

---
+++ CabBuddies-TravelGroups/src/main/java/com/cabbuddies/CabBuddiesTravelGroupsApplication.java
@@ -0,0 +1,13 @@
+package com.cabbuddies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabBuddiesTravelGroupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabBuddiesTravelGroupsApplication.class, args);
+	}
+
+}
